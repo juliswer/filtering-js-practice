9e363cae39dc01b88b8d674532f0a7e331aa1a82
juliswer <julianswer36@gmail.com>
2022-05-08 09:30:03 -0300

    base files

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#searchInput")
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded")
+})
+
+input.addEventListener("keyup", (e) => {
+    console.log(e)
+})
