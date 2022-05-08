4bf73799215b8fec443908592a383582788ccde0
juliswer <julianswer36@gmail.com>
2022-05-08 09:44:32 -0300

    uploading script

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const input = document.querySelector("#searchInput")
+const input = document.querySelector("#searchInput");
+const userList = document.querySelector("#users");
 
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded")
-})
+async function loadUsers() {
+  const response = await fetch(
+    "https://fakerapi.it/api/v1/users?_quantity=1000"
+  );
+  return await response.json();
+}
+
+const createUserItems = (users) =>
+  users
+    .map(
+      (user) => `
+    <li>
+        ${user.firstname} ${user.lastname}
+    </li>
+`
+    )
+    .join(" ");
+
+function renderUsers(users) {
+  const itemsString = createUserItems(users);
+  userList.innerHTML = itemsString;
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const data = await loadUsers();
+  renderUsers(data.data);
+});
 
 input.addEventListener("keyup", (e) => {
-    console.log(e)
-})
+  console.log(input.value);
+});
